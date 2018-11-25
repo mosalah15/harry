96b778e04610ff318c8c0070e4da52e2668b5d94
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-25 18:32:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "d";
 var adminprefix = '-'
-const developers = ["516213327005745154"]
+const developers = ["348883739738112004"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
