84cde0561761643cceb544bf18fc25e882799dd9
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-25 21:24:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const developers = ["348883739738112004"]
-const adminprefix = "^";
+const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
@@ -16,7 +16,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**Status You  ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
+  if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
       message.channel.send(`**Status You ${argresult} **`)
 }
